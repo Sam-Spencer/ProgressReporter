cfc9a4cbf42e7a43f2a1d31e91b0d280da514c32
Sam Spencer <samuels5@mail.usf.edu>
2020-05-22 23:55:01 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # ProgressReporter
 
-A description of this package.
+A highly abstract method to coordinate input from multiple objects on the progress of a single, complex task.
+
+## Why?
+There's really not much to it. If your project uses Swift and has complex or long-running tasks, try this out. **Maybe you won't have to reinvent the wheel.** I use it in my own projects for networking operations and Core Data migrations as well as complex data tasks... basically anything the user needs to see a progress bar for.
+
+## Installation
+There's only one file. You can drag and drop it into your project as use as needed. Or if you're feeling fancy / lazy / extravagant you can use Swift Package Manager.
+
+## Getting Started
+Inline documentation is verbose and helpful, but if you're not sure where to get started, here's a rundown of things:
+
+### Most use-cases
+ 1. Create a `ProgressWatcher` and use the `shared` instance in places you need to report progress.
+ 2. Conform a view or controller to the `ProgressReporter` protocol and then set the `reporter` object on your Watcher.
+ 3. Add as many steps to a `Progress` object as needed using `addStepsToProgress`.  
+ 4. Each time a "step" completes, call `reportProgress` on your shared instance.
+ 5. That's it. `hasProgressToReport` will be called on your `ProgressReporter` each time an update occurs. Check the `progress` value of the `Progress` struct for a float betwee 0.0 and 1.0. Perfect for a `UIProgressView`!
+
+### More complex cases
+The shared instance model may not make sense for you. You can simply avoid accessing the `shared` instance and initialize in your own way. 
+
+You can subclass the `ProgressWatcher` or implement the `ProgressCensus` protocol. Either way, your flexibility increases greatly. The drawback (or potentially, benefit) with implementing the protocol is that you must perform progress calcualtions on your own.
--- Sources/ProgressReporter/ProgressReporter.swift
+++ Sources/ProgressReporter/ProgressReporter.swift
@@ -1,3 +1,140 @@
-struct ProgressReporter {
-    var text = "Hello, World!"
+//
+//  ProgressReporter.swift
+//  LessonNote
+//
+//  Created by Sam Spencer on 3/2/20.
+//  Copyright © 2020 LSAlliance. All rights reserved.
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this
+//  software and associated documentation files (the "Software"), to deal in the Software
+//  without restriction, including without limitation the rights to use, copy, modify, 
+//  merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+//  permit persons to whom the Software is furnished to do so, subject to the following 
+//  conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in all copies
+//  or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+//  PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+//  FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+//  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+//  DEALINGS IN THE SOFTWARE.
+// 
+
+import Foundation
+
+
+/// A mutable representation of tangible progress
+/// 
+/// Does not discern between continuous or discrete progress.
+/// Create this in a `ProgressReporter` and use a `ProgressWatcher`
+/// for monitoring.
+///
+public struct Progress {
+    
+    /// Representation of progress towards completion.
+    /// This value is capped at the value of `total`
+    public var completed: Int {
+        didSet {
+            if total < completed {
+                total = completed
+            }
+        }
+    }
+    
+    /// Representation of total expected progress until completion.
+    public var total: Int {
+        didSet {
+            if total < completed {
+                total = completed
+            }
+        }
+    }
+    
+    /// Current percent towards completion from 0.0 to 1.0
+    public var progress: Float {
+        get {
+            return Float(completed / total)
+        }
+    }
+    
+}
+
+
+/// Objects which coordinate progress on long-running tasks should
+/// adopt this protocol.
+///
+/// - note: Adopting this protocol yourself is likely unnecessary.
+///   Instead, use the `ProgressWatcher` object.
+/// 
+public protocol ProgressCensus {
+    func reportProgress()
+    func addStepsToProgress(additionalSteps: Int)
+}
+
+
+/// A Progress Reporter is responsible for recieving updates
+/// from a Progress Census and deciding how to proceed with the
+/// given information (i.e. updating a UI).
+public protocol ProgressReporter {
+    func hasProgressToReport(report: Progress)
+}
+
+
+/// Coordinates input from multiple objects on the progress of a single task.
+/// 
+/// Only one task may be active at any given time. The benefit, however,
+///  is that multiple objects may access this same Census and provide updates
+///  asynchronously.
+/// 
+/// If you need multiple different tasks reporting different progress values, 
+/// you should either: 
+///   - Conform to the `ProgressCensus` protocol in your own custom object.  
+///   - Subclass this class to take advantage of multiple *shared* Census coordinators.
+///   - Avoid use of the `shared` property and instead initialize in a traditional manner,
+///     thus maintaining your own references and ensuring multiple instances of 
+///     `ProgressWatcher` can be generated at once.
+/// 
+open class ProgressWatcher: ProgressCensus {
+    
+    public static let shared = ProgressWatcher()
+    public var reporter: ProgressReporter?
+    
+    public var progress: Progress {
+        get {
+            return Progress.init(completed: completedSteps, total: totalSteps)
+        }
+    }
+    
+    private var completedSteps = 0
+    private var totalSteps = 1 {
+        didSet {
+            if totalSteps < 1 {
+                totalSteps = 1
+            }
+        }
+    }
+    
+    public func reportProgress() {
+        completedSteps = completedSteps + 1
+        updateReporterSafely()
+    }
+    
+    public func addStepsToProgress(additionalSteps: Int) {
+        totalSteps = totalSteps + additionalSteps
+        updateReporterSafely()
+    }
+    
+    private func updateReporterSafely() {
+        if Thread.isMainThread {
+            reporter?.hasProgressToReport(report: progress)
+        } else {
+            DispatchQueue.main.async {
+                self.reporter?.hasProgressToReport(report: progress)
+            }
+        }
+    }
+    
 }
