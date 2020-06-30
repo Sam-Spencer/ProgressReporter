a1997f07c89572b581dfd6ef5666887eabbfe7b8
Sam Spencer <samuels5@mail.usf.edu>
2020-06-30 19:23:54 -0400

    Added platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ProgressReporter",
+    platforms: [
+        .iOS("13.0")
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
